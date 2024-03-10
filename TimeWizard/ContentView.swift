eb431b6073a906951ee28ea219bad97d2e2b23a6
Ahsan <ahsan.qureshi8@gmail.com>
2024-03-10 19:12:14 -0400

    TimeWizard time conversion app finished

--- TimeWizard/ContentView.swift
+++ TimeWizard/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var startingUnit = "Seconds"
+    @State private var endingUnit = "Seconds"
+    @State private var inputTime = 0.0
+    
+    private let timeUnits = ["Seconds", "Minutes", "Hours", "Days"]
+    private let unitConversion = [60, 60, 24, 1]
+    private var outputTime: Double {
+        var convertedValue = inputTime
+        var startingIndex = timeUnits.firstIndex(of: startingUnit) ?? 0
+        let endingIndex = timeUnits.firstIndex(of: endingUnit) ?? 0
+        while (startingIndex != endingIndex) {
+            if (startingIndex > endingIndex) {
+                startingIndex -= 1
+                convertedValue *= Double(unitConversion[startingIndex])
+            } else {
+                convertedValue /= Double(unitConversion[startingIndex])
+                startingIndex += 1
+            }
         }
-        .padding()
+        return convertedValue
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Starting Unit") {
+                    Picker("Starting Unit", selection: $startingUnit) {
+                        ForEach(timeUnits, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Converted Unit") {
+                    Picker("Converted Unit", selection: $endingUnit) {
+                        ForEach(timeUnits, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Time to convert (\(startingUnit))") {
+                    TextField("\(startingUnit)", value: $inputTime, format: .number)
+                        .keyboardType(.numberPad)
+                }
+                Section("Converted time (\(endingUnit))") {
+                    Text(outputTime, format: .number)
+                }
+                
+            }
+            .navigationTitle("Time Converter")
+
+        }
     }
 }
 
