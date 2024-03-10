786a5e38670183f6c9987c915bc29c2c8e5e8564
Ahsan <ahsan.qureshi8@gmail.com>
2024-03-10 16:30:12 -0400

    Initial Commit

---
+++ TimeWizard/TimeWizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeWizardApp.swift
+//  TimeWizard
+//
+//  Created by Ahsan Qureshi on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeWizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
